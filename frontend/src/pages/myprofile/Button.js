0457ccbb1909e493b0c10ca16b9c8d0204c078fe
Nufarr <paraschivandreea1122@gmail.com>
2023-09-07 00:01:20 +0300

    BoxSpinner

--- frontend/src/pages/myprofile/Button.js
+++ frontend/src/pages/myprofile/Button.js
@@ -1,8 +1,21 @@
 import Spinner from 'react-bootstrap/Spinner';
 import './Button.css';
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function MyButton(){
     return (
-        <div className='divspinner'><Spinner className='spinner'/></div>
+        <ClimbingBoxLoader
+            color="#a824c1"
+            cssOverride={{
+                display: 'block',
+                margin: '20% auto 40%'
+              }}
+            size={60}
+            />
+            
+
     );
 }
